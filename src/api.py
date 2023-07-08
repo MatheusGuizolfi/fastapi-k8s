7e51ed8b5352e0b168daea5c5f76f613c643ad1c
Matheus Pereira <matheus.pereira@coxautoinc.com>
2023-07-08 20:35:18 -0300

    Create base infrastructure for API

--- src/api.py
+++ src/api.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"message": "Hello Matheus"}
+
+@app.get("/hello/{name}")
+def hello_person(name):
+    return {"message": "Hello {}".format(name)}
