a85b0e9700227964e8d3d77dac1027a84e402fcb
Matheus Guizolfi <matheusguizolfi@hotmail.com>
2023-07-08 20:45:03 -0300

    Remove test route

--- src/api.py
+++ src/api.py
@@ -5,7 +5,3 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"message": "Hello Matheus"}
-
-@app.get("/hello/{name}")
-def hello_person(name):
-    return {"message": "Hello {}".format(name)}
